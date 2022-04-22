1ccf8f7064e0d855df292f7835acf443d2cbeef8
tim <thogg4@gmail.com>
2022-04-22 01:21:37 -0400

    add file i missed

--- test/login/view/login_page_test.dart
+++ test/login/view/login_page_test.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_login/login/view/view.dart';
+import 'package:flutter_login/repositories/user_repository/lib/src/user_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:user_repository/src/user_repository.dart';
 
 class MockUserRepository extends Mock implements UserRepository {}
 
